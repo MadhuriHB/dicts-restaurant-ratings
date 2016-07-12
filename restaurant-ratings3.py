ee951b09693dcd104ddeadc1b33a7d3dac68c6ff
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:49:24 -0700

    new stuff

---
+++ restaurant-ratings3.py
@@ -0,0 +1,36 @@
+from random import choice
+
+def restaurant_ratings(filename):
+
+    myDict = {}
+    input_file = open(filename)
+    for line in input_file:
+        line = line.rstrip()
+        words = line.split(":")
+        myDict[words[0]] = words[1]  
+
+    while True:
+        
+        query_restaurant = raw_input("Please enter a restaurant name or 'q' to quit: ")
+
+        if query_restaurant == 'q':
+            break
+
+        else:     
+
+            query_rating = int(raw_input("Please enter the restaurant rating: "))
+        
+
+            myDict[query_restaurant] = query_rating  
+            myKeys = sorted(myDict)
+            #random_restaurant = choice(myKeys)
+
+            for key in myKeys:
+                print "%s is rated at %s." %(key, myDict[key])
+            input_file.close()
+
+restaurant_ratings("scores.txt")
+
+
+
+
